bd6c86f461f1df692bd6a43f7d14c5617046154c
Kaz Matthews and Sandro Mijatovic <kaz+sandro@>
2017-03-21 14:31:44 -0700

    add index view with each article title, image, and author.

---
+++ config/environment.js
@@ -0,0 +1,58 @@
+/* eslint-env node */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'bustle',
+    environment: environment,
+    rootURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
+      }
+    },
+
+    firebase: {
+      apiKey: "AIzaSyC6XXYo1s4kmyyH7dzSbS5do75B2YB4mUk",
+      authDomain: "articles-e904c.firebaseapp.com",
+      databaseURL: "https://articles-e904c.firebaseio.com",
+      storageBucket: "articles-e904c.appspot.com",
+      messagingSenderId: "298310561106"
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
